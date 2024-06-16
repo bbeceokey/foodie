67a307132c69a3a8926980d39906cc2d6abe74b9
Büşra Ece Ok <bbece117@gmail.com>
2024-06-16 08:21:16 +0300

    created project and exist network manager

---
+++ foodie/foodie/Network Protocol/Network Manager.swift
@@ -0,0 +1,113 @@
+//
+//  Network Manager.swift
+//  foodie
+//
+//  Created by Busra Ece on 16.06.2024.
+//
+
+import Alamofire
+import Foundation
+
+typealias NetworkError = AFError
+
+enum RequestStatus {
+    case loading, success, failure
+}
+
+class NetworkManager {
+    static let instance: NetworkManager = .init()
+
+    private let baseUrl = "http://localhost:8222" // value from env var or config
+    private let userDefaults = UserDefaults.standard
+
+    func get<Output: Decodable>(_ url: String,
+                                output _: Output.Type,
+                                completion: @escaping (Result<Output, NetworkError>) -> Void)
+    {
+        AF.request(baseUrl + url, interceptor: self).validate().responseDecodable(of: Output.self) { response in
+
+            completion(response.result)
+        }
+    }
+
+    func delete<Output: Decodable>(_ url: String,
+                                   output _: Output.Type,
+                                   completion: @escaping (Result<Output, NetworkError>) -> Void)
+    {
+        AF.request(baseUrl + url, method: .delete,
+                   interceptor: self).validate().responseDecodable(of: Output.self) { response in
+
+            completion(response.result)
+        }
+    }
+
+    func post<Input: Encodable, Output: Decodable>(_ url: String,
+                                                   input: Input,
+                                                   output _: Output.Type,
+                                                   completion: @escaping (Result<Output, NetworkError>) -> Void)
+    {
+        AF.request(baseUrl + url,
+                   method: .post,
+                   parameters: input,
+                   encoder: .json,
+                   headers: [
+                    .init(name: "Content-Type", value: "application/json"),
+                   ],
+                   interceptor: self)
+        .validate()
+        .responseDecodable(of: Output.self) { response in
+            completion(response.result)
+        }
+    }
+
+    func put<Input: Encodable, Output: Decodable>(_ url: String,
+                                                  input: Input,
+                                                  output _: Output.Type,
+                                                  completion: @escaping (Result<Output, NetworkError>) -> Void)
+    {
+        AF.request(baseUrl + url,
+                   method: .put,
+                   parameters: input,
+                   encoder: .json,
+                   headers: [
+                    .init(name: "Content-Type", value: "application/json"),
+                   ],
+                   interceptor: self)
+        .validate()
+        .responseDecodable(of: Output.self) { response in
+            completion(response.result)
+        }
+    }
+
+    func patch<Input: Encodable, Output: Decodable>(_ url: String,
+                                                    input: Input,
+                                                    output _: Output.Type,
+                                                    completion: @escaping (Result<Output, NetworkError>) -> Void)
+    {
+        AF.request(baseUrl + url,
+                   method: .patch,
+                   parameters: input,
+                   encoder: .json,
+                   headers: [
+                    .init(name: "Content-Type", value: "application/json"),
+                   ],
+                   interceptor: self)
+        .validate()
+        .responseDecodable(of: Output.self) { response in
+            completion(response.result)
+        }
+    }
+}
+
+extension NetworkManager: RequestInterceptor {
+    func adapt(_ urlRequest: URLRequest, for _: Session, completion: @escaping (Result<URLRequest, Error>) -> Void) {
+        var request = urlRequest
+
+        if let token = userDefaults.string(forKey: "SUPER_SECURE_JWT") {
+            let bearerToken = "Bearer \(token)"
+            request.setValue(bearerToken, forHTTPHeaderField: "Authorization")
+        }
+
+        completion(.success(request))
+    }
+}
