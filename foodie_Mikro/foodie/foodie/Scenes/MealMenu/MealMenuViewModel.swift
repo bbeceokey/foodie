9a96cfdc929e589393f527fa265cfd0724e1fcd0
Büşra Ece Ok <bbece117@gmail.com>
2024-06-20 02:40:47 +0300

    planning

--- foodie_Mikro/foodie/foodie/Scenes/MealMenu/MealMenuViewModel.swift
+++ foodie_Mikro/foodie/foodie/Scenes/MealMenu/MealMenuViewModel.swift
@@ -9,7 +9,12 @@ import Foundation
 
 
 protocol MealMenuViewModelProtocol{
-    
+    //TODO: meallar be den çekilecek
+    //TODO: cell configure edilecek -> bunun için delegate gerekebilir
+    //TODO: searchController eklenicek -> bunun için delegate gerekebilir
+    //TODO: see all btn route edicek
+    //TODO: cellere tıklanınca detay sayfaları gelicek
+    //TODO: image e profil resmi gelicek ve tıklanınca profil sayfasına route edicek
 }
 
 extension MealMenuViewModelProtocol{
